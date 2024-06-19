365da3cfd923ef3676bc321d7b9e9d79c69ceb1e
MillerTGr <MillerTGr@users.noreply.github.com>
2024-06-19 18:46:01 +1000

    Initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function create()
+    {
+        return view('user.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'max:255'],
+            'email' => ['required', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'min:8', 'confirmed']
+        ]);
+
+        $user = User::create($request->all());
+        event(new Registered($user));
+        Auth::login($user);
+
+        # return redirect()->route('login')->with('success', 'Successfully registration');  # С сообщением об успешной регистрации
+        return redirect()->route('verification.notice');
+    }
+
+    public function login()
+    {
+        return view('user.login');
+    }
+
+    public function loginAuth(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials, $request->boolean('remember'))) {
+            $request->session()->regenerate();
+            return redirect()->intended('dashboard')->with('success', 'Welcome, ' . Auth::user()->name . '!');
+        }
+
+        return back()->withErrors([
+            'email' => 'Wrong email or password',
+        ]);
+
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->route('login');
+    }
+
+    public function dashboard()
+    {
+        return view('user.dashboard');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', [UserController::class, 'dashboard'])->name('dashboard');
+});
+
+Route::middleware('guest')->group(function () {
+    Route::get('register', [UserController::class, 'create'])->name('register');
+    Route::post('register', [UserController::class, 'store'])->name('user.store');
+
+    Route::get('login', [UserController::class, 'login'])->name('login');
+    Route::post('login', [UserController::class, 'loginAuth'])->name('login.auth');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('verify-email', function () {
+        return view('user.verify-email');
+    })->name('verification.notice');
+
+    Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+        $request->fulfill();
+
+        return redirect()->route('dashboard');
+    })->middleware('signed')->name('verification.verify');
+
+    Route::post('/email/verification-notification', function (Request $request) {
+        $request->user()->sendEmailVerificationNotification();
+
+        return back()->with('message', 'Verification link sent!');
+    })->middleware('throttle:2,1')->name('verification.send');  # 2 запроса в минуту
+
+    Route::get('logout', [UserController::class, 'logout'])->name('logout');
+});
